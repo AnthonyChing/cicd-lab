76bd1ada5fda4d4177a790fbb05e1dc31e54714d
AnthonyChing <75062396+AnthonyChing@users.noreply.github.com>
2026-05-18 15:17:13 +0000

    Added vitest-coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,7 +6,11 @@ type VitestConfig = ViteUserConfig & {
 
 const config: VitestConfig = {
   test: {
-    exclude: ['dist/**', 'node_modules/**']
+    exclude: ['dist/**', 'node_modules/**'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'json-summary'],
+    }
   }
 };
 
